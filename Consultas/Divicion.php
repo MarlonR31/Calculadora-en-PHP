fc13577efc0f7be196cce7b40f62d0e0f74042c8
MarlonR31 <127542854+MarlonR31@users.noreply.github.com>
2024-10-12 10:59:47 -0600

    Add files via upload

---
+++ Consultas/Divicion.php
@@ -0,0 +1,29 @@
+<?php
+
+echo "Dividiendo ".$_POST["Numero1"];
+echo " / ".$_POST["Numero2"];
+echo "<br>";
+
+class Multiplicacion
+{
+    public $Num1;
+    public $Num2;
+
+    public function __construct($Numero1, $Numero2)
+    {
+        $this->Num1 = $Numero1;
+        $this->Num2 = $Numero2;
+    }
+
+    public function operacion()
+    {
+        $Resultado = $this->Num1 / $this->Num2;
+        echo "La Divicion de ".$this->Num1. " / ". $this->Num2. " es igual a: ". $Resultado;
+    }
+
+}
+
+$Operacion = new Multiplicacion($_POST['Numero1'], $_POST["Numero2"]);
+$Operacion->operacion()
+
+?>
